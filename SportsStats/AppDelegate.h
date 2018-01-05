e23cbb84e1d1dd745a3eb32933e88e0abd03a3e2
apple <apple@apples-macbook-pro.local>
2018-01-05 16:57:48 +0530

    Initial Commit

---
+++ SportsStats/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SportsStats
+//
+//  Created by apple on 05/01/18.
+//  Copyright © 2018 agaram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
